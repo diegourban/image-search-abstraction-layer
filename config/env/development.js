2fe1463d751903fa1da28f900199bde93637ace5
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-11-24 09:15:17 -0200

    CSE context

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,9 @@ module.exports = {
   db: 'mongodb://mongodb:27017/image-search',
   google: {
     images: {
-      cseId: 'change_it',
-      apiKey: 'change_it'
+      // freecodecamp sandbox account info, it may be disabled when you try to use ;)
+      cseId: '001543945358577552852:llyskds2xem',
+      apiKey: 'AIzaSyAkld83wbaDqu2JUjlhQhVvx_CGAf6Al5Y'
     }
   }
 };
--- docker/image-search.dockerfile
+++ docker/image-search.dockerfile
@@ -1,5 +1,5 @@
 FROM node:latest
-MAINTAINER Diego Leonardo Urban
+MAINTAINER diegourban
 ENV NODE_ENV=development
 COPY . /var/www
 WORKDIR /var/www
