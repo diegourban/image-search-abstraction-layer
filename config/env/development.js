9b3061101dee2d5608879e4b91f53ffe8d0a70cb
Diego L. Urban <diego.urban88@gmail.com>
2017-11-24 08:41:46 -0200

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,8 @@ module.exports = {
   db: 'mongodb://mongodb:27017/image-search',
   google: {
     images: {
-      cseId: '010979884837995675985:b4umcs_jkpw',
-      apiKey: 'AIzaSyA3mT9h8VfXMe-ctGxs7y1Ca31vx-fMgEQ'
+      cseId: 'change_it',
+      apiKey: 'change_it'
     }
   }
 };
